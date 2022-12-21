8cddb4383c097d42e270f1b75646f7b4ab88eb03
bholcomb <bholcomb@nvidia.com>
2022-12-21 11:04:54 -0500

    Updating to Lua 5.4.4

--- build/premake5.lua
+++ build/premake5.lua
@@ -27,43 +27,42 @@ project "lua5.4"
 	kind "SharedLib"
 	language "C"
 	location "luadll"
-	files{"../lua-5.4.0/src/*.h", "../lua-5.4.0/src/*.c"}
-	excludes {"../lua-5.4.0/src/luac.c", "../lua-5.4.0/src/lua.c"}
+	files{"../lua-5.4.4/src/*.h", "../lua-5.4.4/src/*.c"}
+	excludes {"../lua-5.4.4/src/luac.c", "../lua-5.4.4/src/lua.c"}
 	
 project "luac"
 	kind "ConsoleApp"
 	language "C"
 	location "luac"
 	files{
-   "../lua-5.4.0/src/luac.c", 
-   "../lua-5.4.0/src/lopcodes.c",
-   "../lua-5.4.0/src/lopcodes.h",
-   "../lua-5.4.0/src/ldump.c",
-   "../lua-5.4.0/src/ldebug.c",
-   "../lua-5.4.0/src/lobject.c",
-   "../lua-5.4.0/src/lvm.c",
-   "../lua-5.4.0/src/ltm.c",
-   "../lua-5.4.0/src/ldo.c",
-   "../lua-5.4.0/src/lstate.c",
-   "../lua-5.4.0/src/ltable.c",
-   "../lua-5.4.0/src/lmem.c",
-   "../lua-5.4.0/src/lgc.c",
-   "../lua-5.4.0/src/lfunc.c",
-   "../lua-5.4.0/src/lzio.c",
-   "../lua-5.4.0/src/lparser.c",
-   "../lua-5.4.0/src/lundump.c",
-   "../lua-5.4.0/src/llex.c",
-   "../lua-5.4.0/src/lcode.c",
-   "../lua-5.4.0/src/lctype.c",
-   "../lua-5.4.0/src/lstring.c"}
-   
-	links {"lua5.4"}
+   "../lua-5.4.4/src/luac.c", 
+   "../lua-5.4.4/src/lopcodes.c",
+   "../lua-5.4.4/src/lopcodes.h",
+   "../lua-5.4.4/src/ldump.c",
+   "../lua-5.4.4/src/ldebug.c",
+   "../lua-5.4.4/src/lobject.c",
+   "../lua-5.4.4/src/lvm.c",
+   "../lua-5.4.4/src/ltm.c",
+   "../lua-5.4.4/src/ldo.c",
+   "../lua-5.4.4/src/lstate.c",
+   "../lua-5.4.4/src/ltable.c",
+   "../lua-5.4.4/src/lmem.c",
+   "../lua-5.4.4/src/lgc.c",
+   "../lua-5.4.4/src/lfunc.c",
+   "../lua-5.4.4/src/lzio.c",
+   "../lua-5.4.4/src/lparser.c",
+   "../lua-5.4.4/src/lundump.c",
+   "../lua-5.4.4/src/llex.c",
+   "../lua-5.4.4/src/lcode.c",
+   "../lua-5.4.4/src/lctype.c",
+   "../lua-5.4.4/src/lstring.c"}
+   links {"lua5.4"}
  
 project "lua"
 	kind "ConsoleApp"
 	language "C"
 	location "lua-exe"
-	files{"../lua-5.4.0/src/lua.c"}
+	files{"../lua-5.4.4/src/lua.c"}
 	links{"lua5.4"}
  
 project "Lua#"
